ebb33c65ff773a8393082af5435877633cc329fb
Vu Tran <vutran6853@gmail.com>
2020-07-14 04:12:18 -0600

    add layout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -75,7 +75,6 @@ export default {
           message: 'Please enter new PIN.'
         })
       }
-     
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import AtComponents from 'at-ui'
+
 import 'at-ui-style'    // Import CSS
 
 Vue.use(AtComponents)
